1707fa5ab176a19a117e22cbb602cc4761f66cfd
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2025-01-31 01:54:10 +0530

    setup loader and home page

---
+++ components/IntroLoader.tsx
@@ -0,0 +1,10 @@
+import { View, ActivityIndicator, Text } from "react-native";
+
+const IntroLoader: React.FC = () => (
+  <View className="flex-1 justify-center items-center bg-white">
+    <ActivityIndicator size="large" className="text-blue-500" />
+    <Text className="mt-4 text-lg text-gray-600">Loading...</Text>
+  </View>
+);
+
+export default IntroLoader;
