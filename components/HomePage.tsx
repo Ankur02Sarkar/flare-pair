bcedf0280743ff7754c548c6ae9692159943b39d
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2025-02-05 16:35:02 +0530

    added font

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -3,15 +3,11 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import AppLoading from "expo-app-loading";
 import { useFonts, Pacifico_400Regular } from "@expo-google-fonts/pacifico";
-import { LinearGradient } from "react-native-svg";
 
 export default function HomePage() {
   let [fontsLoaded] = useFonts({
     Pacifico_400Regular,
   });
-
-  let fontSize = 24;
-  let paddingVertical = 6;
 
   if (!fontsLoaded) {
     return <AppLoading />;
@@ -20,14 +16,13 @@ export default function HomePage() {
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text
           style={{
-            fontSize,
+            fontSize: 70,
+            paddingVertical: 6,
             color: "white",
-            paddingVertical,
-            // Note the quoting of the value for `fontFamily` here; it expects a string!
             fontFamily: "Pacifico_400Regular",
           }}
         >
-          Pacifico Regular
+          FlarePair
         </Text>
       </View>
     );
