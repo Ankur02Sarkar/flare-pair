e663ebcc613a54072a741f64470d5535bda7a986
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2025-02-06 00:25:24 +0530

    added starter custom login register reset screens

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,12 +1,16 @@
-import React from "react";
 import { View, Text } from "react-native";
+import Login from "./AuthScreen/Login";
+import React = require("react");
+import Register from "./AuthScreen/Register";
+import PwReset from "./AuthScreen/Reset";
 
 const HomePage: React.FC = () => (
   <View className="flex-1 justify-center items-center bg-gray-50 p-4">
-    <Text className="text-2xl font-bold text-gray-800 mb-4">Welcome Home!</Text>
+    {/* <Text className="text-2xl font-bold text-gray-800 mb-4">Welcome Home!</Text>
     <Text className="text-gray-600 text-center">
       This is your main application screen
-    </Text>
+    </Text> */}
+    <PwReset />
   </View>
 );
 
